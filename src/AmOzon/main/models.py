de0e65d98ea0318ac13dea4dde2554c04239b090
anjelicm <anjelicm@student.21-school.ru>
2023-03-20 00:21:15 +0300

    [FireG45] Добавлено расширение шаблонов и таблица товаров

--- src/AmOzon/main/models.py
+++ src/AmOzon/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(verbose_name="Наименование товара", max_length=250)
+    description = models.TextField(verbose_name="Описание товара")
+    parameters = models.TextField(verbose_name="Дополнительная информация")
+    price = models.PositiveBigIntegerField(verbose_name="Цена товара")
+    category = models.CharField(verbose_name="Категория товара", max_length=250)
+    image = models.URLField(verbose_name="Ссылка на изображение товара")
